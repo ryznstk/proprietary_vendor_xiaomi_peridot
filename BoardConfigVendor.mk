2ee6f7e47030ba950abf5290898a472fd39d4607
ryznstk <agusmhrd1k4@gmail.com>
2026-08-17 06:03:02 +0000

    revert firmware

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -2,28 +2,3 @@
 # Automatically generated file. DO NOT MODIFY
 #
 
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    aop_config \
-    bluetooth \
-    countrycode \
-    cpucp \
-    cpucp_dtb \
-    devcfg \
-    dsp \
-    featenabler \
-    hyp \
-    imagefv \
-    keymaster \
-    modem \
-    modemfirmware \
-    multiimgqti \
-    qupfw \
-    shrm \
-    tz \
-    uefi \
-    uefisecapp \
-    xbl \
-    xbl_config \
-    xbl_ramdump
