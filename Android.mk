ff946c75476dfdcd2ffb89535b0bb8c5b4e262fd
ryznstk <agusmhrd1k4@gmail.com>
2026-08-22 12:43:15 +0700

    remove unused radio images and related entries from build configuration

--- Android.mk
+++ Android.mk
@@ -3,32 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),peridot)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,545cbd9797eb3773c02c777fa1afa1d325d6add6)
-$(call add-radio-file-sha1-checked,radio/aop.img,6d1a6818e9fbf59935cb5fb5d588358d776c8ace)
-$(call add-radio-file-sha1-checked,radio/aop_config.img,bf1c0411e2cba12ae1d4168209852d414023d7d0)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,cd6d88e5e875c7f33b4a67c05c4e8192f8c06973)
-$(call add-radio-file-sha1-checked,radio/countrycode.img,fa5535938b8e1b1b2f715402dd8243f6815cbbd2)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,05803e0a5cb7305ec81072f4b560aa2a4a21f07c)
-$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,4e41e6141f5ca2940bf48217a7fc718f0cff9bda)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,78442980a6ce24f72af7bebdcf4dd0f5132b3769)
-$(call add-radio-file-sha1-checked,radio/dsp.img,2814e90c852ad8c7e4afab34a41d1be74cfb0015)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,3d2f35afd4ef90ce51b9584f0705a81972753409)
-$(call add-radio-file-sha1-checked,radio/hyp.img,4a9cc009c978363004cacd4a884c22cb39a15798)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,651810fed2f10c5577b0140eab9e55469a251d86)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,7a3d2c1e0b5c2c56020b774035f758ef5a157e61)
-$(call add-radio-file-sha1-checked,radio/modem.img,20e48b4570d1f0fd361dcb947fc2ea04bf962d1f)
-$(call add-radio-file-sha1-checked,radio/modemfirmware.img,1e3d2fa44b7e423acb50e78fcdb7a14a8c4203ac)
-$(call add-radio-file-sha1-checked,radio/multiimgqti.img,85c6e03d0dd54630f5017c9c83517fbe19ad6af1)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,9cc9902775c5d7e27e48220c3aec8dedf93f8c3e)
-$(call add-radio-file-sha1-checked,radio/shrm.img,a5c10641aa445b0e3995c16fa2aaddc43661313f)
-$(call add-radio-file-sha1-checked,radio/tz.img,55d016e27eb04b1ea851243d6d07805e1d43880f)
-$(call add-radio-file-sha1-checked,radio/uefi.img,c5c85e16f424068daeca7e25a5b3246428d144eb)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,a746d2062f695356bac0b3f68f70d11a78d8d84f)
-$(call add-radio-file-sha1-checked,radio/xbl.img,d54337d55d837182be59ce530bb53df300bdf3fc)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,4f927d13d8d894ca7d1070f1854479490a799f05)
-$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,2be5443a106e428cd540490d94f7e8fbe4c6ffaa)
-
-endif
